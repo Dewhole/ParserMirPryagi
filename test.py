b628da2cf3e5d0fd9ae52b54f310775f79d9a692
User <dewhole@ubuntu.ubuntu-domain>
2020-11-09 12:50:55 +0700

    csvRewrite

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import csv
 import time
 import urllib
 
-URL = 'https://www.mir-priaji.ru/catalog/aksessuary_dlya_vyshivaniya/'
+
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0', 'accept': '*/*'}
 HOST = 'https://www.mir-priaji.ru/'
 FILE = 'catalog.csv'
@@ -19,7 +19,6 @@ def get_pages_count(html):
     soup = BeautifulSoup(html, 'html.parser')
     paginationTo = soup.find('div', class_='navigation-pages')
     pagination = paginationTo.find_all('a')
-    print(pagination)
     if pagination:
         return int(pagination[-1].get_text())
     else:
@@ -42,7 +41,7 @@ def get_content(html):
 
         catalog.append({
             'title': item.find('a', class_='dark_link').get_text(strip=True),
-
+            'kategory': soup.find('h1', id='pagetitle').get_text(strip=True),
             'image': HOST + (image),
             'kol-vo': item.find('span', class_='value').get_text(strip=True),
         })
@@ -52,31 +51,31 @@ def get_content(html):
 def save_file(items, path):
     with open(path, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
-        writer.writerow(['Название', 'Изображение', 'Наличие',])
+        writer.writerow(['Категория', 'Название', 'Наличие',])
         for item in items:
-            writer.writerow([item['title'], item['image'], item['kol-vo']])
+            writer.writerow([item['kategory'], item['title'], item['kol-vo']])
 
 
 def parse():
-    """ URL = input('Введите URL: ')
-    URL = URL.strip() """
-    html = get_html(URL)
-    if html.status_code == 200:
-        catalog = []
-        pages_count = get_pages_count(html.text)
-        for page in range (1, pages_count + 1):
-            print(f'Парсинг страницы {page} {pages_count}...')
-            html = get_html(URL, params={'page': page})
-            catalog.extend(get_content(html.text))
-            time.sleep(1)
+    for URL in ['https://www.mir-priaji.ru/catalog/aksessuary_dlya_vyshivaniya/', 'https://www.mir-priaji.ru/catalog/applikatsii_termonakleyki/']:
 
-        save_file(catalog, FILE)
+        html = get_html(URL)
+        if html.status_code == 200:
+            catalog = []
+            pages_count = get_pages_count(html.text)
+            for page in range (1, pages_count + 1):
+                print(f'Парсинг страницы {page} {pages_count}...')
+                html = get_html(URL, params={'page': page})
+                catalog.extend(get_content(html.text))
+                time.sleep(1)
+                
+            save_file(catalog, FILE)
 
 
-        print(f'Получено {len(catalog)} товаров')
+            print(f'Получено {len(catalog)} товаров')
 
-    else:
-        print('Error')    
+        else:
+            print('Error')    
 
 
 parse()
